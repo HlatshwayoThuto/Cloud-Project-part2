b341b707c3fe8a25f55ecf5086f2290759eb7954
Thuto Hlatshwayo <hlathuto@gmail.com>
2025-05-12 15:06:08 +0200

    Part2

--- EventEaseApp/obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
+++ EventEaseApp/obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d20b315a08522189de29880849dde482e64aef9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
